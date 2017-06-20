9221aec34324bcab597c853f630856b8e5035638
krzychoooo <krzysztofklimas@interia.pl>
2017-06-20 21:44:15 +0200

    doc folder

--- dp800.h
+++ dp800.h
@@ -50,12 +50,25 @@ public:
     void setI(int, float);
     void setVI(int, float, float);
 	void powerEnable(int, bool);
-	void calibrationStart(int, std::string);
-	void calibrationClear(int, std::string);
+	void calibrationStart(int, std::string);    //chanel , password default "11111"
+	void calibrationClear(int, std::string);    //clears out the current calibration tables  (chanel, "V"|"C"|"ALL")
+
+    /** \brief
+    *\:CALibration:Set <ch>,<mode>,<step>,<tgt_val>,<dev>
+    *\<ch> = "CH1", "CH2", "CH3"
+    *\<mode> = "V" or "C"
+    *\<step> = 0..n
+    *\<tgt_val> ideal value (e.g. 0.0V or 0.1A)
+    *\<dev> : 0 = ADC, 1 = DAC
+     * \param
+     * \param
+     * \return
+     */
 	void calibrationSet(int, std::string, int, float, std::string);
+
 	void calibrationMEAS(int , std::string, int, float, std::string);
-	void calibrationEnd(std::string, int);
-	void displayMode(std::string);
+	void calibrationEnd(std::string, int);      // <date> (e.g. 11/22/2014) , CHANEL
+	void displayMode(std::string);      //{NORMal|WAVE|DIAL|CLAS}
 	void dislayClear (void);
 	void dislayText (std::string, int, int);
     void setText(std::string);
